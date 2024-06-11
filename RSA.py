9d12eebbb7e71397d07f14fd16701350fc0e0891
kota <c0b23048bf@edu.teu.ac.jp>
2024-06-11 15:02:04 +0900

    完成

--- RSA.py
+++ RSA.py
@@ -41,7 +41,46 @@ def anngouka(m, e, n):
     print(f"{s_m}の{e}乗の過程をリスト抽出")
     print(r_l)
     
-    return m % n
+    mstr = str(m)   # mのstring
+    l = len(str(n)) # ビット数
+    waru_dict = []  # 引き算を用いた割り算の筆算の実行結果
+    waru_dict_count = []
+    waru_dict_count_all = []
+    count = 0        # 何回引いたか
+    while True:
+        # 引かれる数先頭の引く数ビット文が引く数よりも大きいとき
+        if int(mstr[0:l]) > n:
+            a = int(mstr[0:l])
+            
+            # 割り算の筆算を行うための引き算実行部
+            while True:
+                count += 1   # if文の中で一回引き算をするので引き算の回数+1
+                # n(引く数)よりも割り算の引き算部分が大きいかどうか
+                if a-n < n:
+                    mstr = str(a-n) + mstr[l:len(mstr)]
+                    waru_dict.append(int(mstr))
+                    waru_dict_count.append(count)
+                    waru_dict_count_all.append(str(count))
+                    count = 0
+                    break
+                a -= n  # 引き算
+                
+        # 大きくない時はビット数を増やす
+        else:
+            l += 1
+            continue
+        
+        # もし、もう割るものがないなら
+        if int(mstr) < n:
+            print("\n以下が割り算の過程である。")
+            print(waru_dict)
+            print("\n以下は割り算の割った数のリストである")
+            print(waru_dict_count)
+            all_count = ''.join(waru_dict_count_all)
+            print(f"\nよって、割られる数:{m} 割る数{n} \n商:{all_count} 余り:{int(mstr)}である")
+            break
+        
+    return int(mstr)
 
 
 # 復号化
@@ -54,7 +93,46 @@ def hukugouka(d, n, c):
     print(f"{s_c}の{d}乗の過程をリスト抽出")
     print(r_l)
     
-    return c % n
+    cstr = str(c)   # mのstring
+    l = len(str(n)) # ビット数
+    waru_dict = []  # 引き算を用いた割り算の筆算の実行結果
+    waru_dict_count = []
+    waru_dict_count_all = []
+    count = 0        # 何回引いたか
+    while True:
+        # 引かれる数先頭の引く数ビット文が引く数よりも大きいとき
+        if int(cstr[0:l]) > n:
+            a = int(cstr[0:l])
+            
+            # 割り算の筆算を行うための引き算実行部
+            while True:
+                count += 1   # if文の中で一回引き算をするので引き算の回数+1
+                # n(引く数)よりも割り算の引き算部分が大きいかどうか
+                if a-n < n:
+                    cstr = str(a-n) + cstr[l:len(cstr)]
+                    waru_dict.append(int(cstr))
+                    waru_dict_count.append(count)
+                    waru_dict_count_all.append(str(count))
+                    count = 0
+                    break
+                a -= n  # 引き算
+                
+        # 大きくない時はビット数を増やす
+        else:
+            l += 1
+            continue
+        
+        # もし、もう割るものがないなら
+        if int(cstr) < n:
+            print("\n以下が割り算の過程である。")
+            print(waru_dict)
+            print("\n以下は割り算の割った数のリストである")
+            print(waru_dict_count)
+            all_count = ''.join(waru_dict_count_all)
+            print(f"\nよって、割られる数:{c} 割る数{n} \n商:{all_count} 余り:{int(cstr)} である")
+            break
+        
+    return int(cstr)
 
 
 if __name__ == '__main__':
